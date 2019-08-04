b065d4e33fed4ad7ea71de643bc7964429cfe40b
Artsiom <lansky1994@gmail.com>
2019-08-04 16:15:37 +0300

    add script.js udemy from mac

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-console.log(console);
-asd
+let num = 93;
+switch (true) {
+    case num <= 49:
+        console.log("Неверно!");
+        break;
+    case num >= 100:
+        console.log("Много!");
+        break;
+    case num > 80:
+        console.log("Все еще много!");
+        break; 
+    case num == 50:
+        console.log("Вthyj");
+        break; 
+    default:
+        console.log("Чтото не так!")
+        break;
+}
+(num == 50) ? console.log('Верно') : console.log('Неверно');
+
